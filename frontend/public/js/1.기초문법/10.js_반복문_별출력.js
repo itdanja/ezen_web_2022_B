5bcc4e2c69775e82a09e5ecafca511ac357aaf7d
이젠강사 <itdanja@kakao.com>
2023-01-12 11:24:12 +0900

    day10 별찍기

--- frontend/public/js/1.기초문법/10.js_반복문_별출력.js
+++ frontend/public/js/1.기초문법/10.js_반복문_별출력.js
@@ -43,6 +43,7 @@ for( let i = 1 ; i<=s2 ; i++ ){ // for s
 	if( i % 3 == 0 ){ output +='\n' }
 } // for e 
 console.log( output )
+
 /*
 
 	만약에  5를 입력했을때 s2 = 5
@@ -59,6 +60,74 @@ console.log( output )
 
 */
 
+/*
+1. 입력받은 줄수[line] 만큼 출력 예시) 5 
+				i[line]		s[star]
+	*			i=1			s = 1
+	**			i=2			s = 1 2 
+	***			i=3			s = 1 2 3 
+	****		i=4			s = 1 2 3 4 
+	*****		i=5			s = 1 2 3 4 5 
+				- i는 1부터 입력받은 수까지 1씩증가 	= for( let i = 1 ; i<=line ; i++ )
+					- s는 1부터 1까지 - s는 1부터 2까지 - s는 1부터 3까지 - s는 1부터 4까지 - s는 1부터 5까지
+				- 결론  s는 1부터 i까지				= for( let s = 1 ; s<=i ; s++ )
+*/
+// 문제1)
+output =''
+let line1 = Number( prompt('문제1 줄수 ') )
+for( let i = 1 ; i<=line1 ; i++ ){ // for s 
+	// 1. 별찍기 
+	for( let s = 1 ; s<=i ; s++ ) { // for s 
+		output += '*' 
+	} // for e  
+	// 2. 줄바꿈 
+	output += '\n'
+} // for e 
+console.log( output )
+
+/*
+2. 입력받은 줄수[line] 만큼 출력  예시) 5 
+			i[line]		s[star]				[ 예시 ]
+	*****	i = 1		s = 1 2 3 4 5 		입력받은 줄수 - 현재줄수+1 : 5 - 1+1	5
+	****	i = 2		s = 1 2 3 4 		입력받은 줄수 - 현재줄수+1 : 5 - 2+1	4
+	***		i = 3 		s = 1 2 3 			입력받은 줄수 - 현재줄수+1 : 5 - 3+1	3
+	**		i = 4		s = 1 2				입력받은 줄수 - 현재줄수+1 : 5 - 4+1	2
+	*		i = 5		s = 1				입력받은 줄수 - 현재줄수+1 : 5 - 5+1	1
+			i는 1부터 입력받은 줄 까지 1씩증가 		= for( let i = 1 ; i<=line2 ; i++ )
+			s는 1부터  (입력받은 줄수 - 현재줄수+1)까지	= for( let s = 1 ; s<= line2-i+1 ; s++ )
+				s는 1부터 5까지		s는 1부터 4까지		s는 1부터 3까지		s는 1부터 2까지		s는 1부터 1까지
+*/
+output = ''
+let line2 = Number( prompt('문제2 줄수') )
+for( let i = 1 ; i<=line2 ; i++ ){ // for s	 : 줄마다 별/줄바꿈 출력
+	// 1. 별 
+	for( let s = 1 ; s<= line2-i+1 ; s++ ){ // for s 
+		output += '*';
+	} // for e
+	// 2. 줄바꿈
+	output += '\n'
+} // for e 
+console.log( output )
+
+/*
+3. 입력받은 줄수[line] 만큼 출력 예시 ) 5	
+
+			i[line]		b[공백]:문제2		s[별]:문제1
+	    *	i = 1		b = 1 2 3 4  	s = 1 
+	   ** 	i = 2		b = 1 2 3 		s = 1 2 
+	  ***	i = 3 		b = 1 2 		s = 1 2 3 
+	 ****	i = 4 		b = 1			s = 1 2 3 4
+	*****	i = 5		b = 			s = 1 2 3 4 5 
+			- i는 1부터 입력받은 줄 까지 1씩 증가 			= for( let i = 1 ; i<=line3 ; i++ )
+			- b는 1부터 (입력받은줄-현재줄수) 까지 1씩증가	= for( let b = 1 ; b<=line3-i ; b++ )
+			- s는 1부터 현재줄수 까지 1씩 증가			= for( let s = 1 ; s<=i ; s++ )
+			!!! 줄마다 공백과 별과 줄바꿈 출력
+			
+			for( let i = 1 ; i<=line3 ;i++ ){
+				for( let b = 1 ; b<=line3-i ; b++ ) { }
+				for( let s = 1 ; s<=i s++ ){ }
+			}
+*/
 
 
 
