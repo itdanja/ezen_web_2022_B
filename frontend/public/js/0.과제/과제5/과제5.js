06c3551cc4dc5d72bc8dc05d136ebb26160e2f44
이젠강사 <itdanja@kakao.com>
2023-01-18 13:50:31 +0900

    day14 키오스크 헤더 끝

--- frontend/public/js/0.과제/과제5/과제5.js
+++ frontend/public/js/0.과제/과제5/과제5.js
@@ -3,20 +3,38 @@
 let categoryList = [ '프리미엄', '스페셜' , '와퍼', '올데이킹','치킨버거']
 
 category_print();
+categoey_select( 0 );
 
 // 1. 카테고리 출력하는 함수 	// [ 1.js열렸을때]
 function category_print(){
-	
 	// 1.HTML 구성
 	let html = `<ul>`
-	
 	//*
 	for( let i = 0 ; i< categoryList.length ; i++ ){
-		html += `<li>${ categoryList[i] }</li>`
+		html += `<li 
+					class="categoryli" 
+					onclick="categoey_select( ${i} )"
+					> ${ categoryList[i] } </li>`
 	} // for end 
 	html+= `</ul>`
-	
 	// 2 해당 마크업에 HTML 출력 
-	document.querySelector('.categorybox').innerHTML = html
-	
+	document.querySelector('.categorybox').innerHTML = html	
+} // f e 
+
+function categoey_select( i ){ // i : 선택된 li의 인덱스
+	// 1. 모든 li 가져와서 배열 저장 
+	let categoryli = document.querySelectorAll( ".categoryli")
+	// 2. 모든 li 배열 반복문 
+	for( let j = 0 ; j<categoryli.length ; j++ ){
+		if( j == i ){ // 만약에 li배열에서 내가 선택횐 li의 인덱스와 같으면
+			categoryli[j].classList.add( 'categoryselect' ) ; // 해당 마크업의 class 식별자 추가 
+		}else{ // 선택되지 않은 li
+			categoryli[j].classList.remove( 'categoryselect'  );// 해당 마크업의 class 식별자 제거 
+		}
+	}
 }
+
+
+
+
+
