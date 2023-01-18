96d83b4744aac66d8f0eab219c61e562cfbfd366
이젠강사 <itdanja@kakao.com>
2023-01-18 10:39:42 +0900

    day14 과제4

--- frontend/public/js/0.과제/과제4/과제4.js
+++ frontend/public/js/0.과제/과제4/과제4.js
@@ -1,8 +1,11 @@
 console.log('js작동') // 연동 확인용
 
+// * JS 열렸을때 실행되는 구역 
 // 1. 배열[ 동일한 데이터유형 저장 ]선언 
 let 도서목록 = [ '혼자공부하는자바' , '이것이자바다' , '열혈 C언어'  ] // 등록된 도서명 목록 
 let 대여목록 = [ '혼자공부하는자바' ] // 등록된 도서명 목록중에 대여중인 도서명 목록
+
+도서현황( 1 );도서현황( 2 );
 
 // 2. 등록 함수 [ 1. 등록버튼 클릭했을때 ]
 function 등록(){
@@ -23,13 +26,12 @@ function 등록(){
 			return false;
 		} // if end 
 	// 3. 입력받은 도서명을 배열에 등록처리 
-	도서목록.push( 도서명 ); 도서현황();
+	도서목록.push( 도서명 ); 도서현황( 1 );도서현황( 2 );
 		console.log( 도서목록 )
 } // f e 
 
-도서현황();
-// 2. 등록된 도서목록 현황 출력 [ 1. JS 열렸을때 2. 등록했을때 3. 삭제했을때 ]
-function 도서현황(){
+// 2. 등록된 도서목록 현황 출력 [ 1. JS 열렸을때 2. 등록했을때 3. 삭제했을때 4.대여/반납 ]
+function 도서현황( 타입 ){
 	// 1. html 기본 구성 
 	let html = `<tr>
 					<th>번호</th> <th>도서명</th>
@@ -41,19 +43,19 @@ function 도서현황(){
 		html += `<tr>
 					<td>${i+1}</td> 
 					<td>${ 도서목록[i] }</td>
-					<td>${ 대여목록.includes( 도서목록[i] ) ? '대여중' : '대여가능' } </td> 
-					<td><button onclick="삭제( ${i} )">삭제</button></td>
-				<tr>`
-							// <button onclick="삭제( ${i} )">삭제</button> : js가 버튼를 생성하기 전
-							/*
-								js가 버튼 생성한 후
-								<button onclick="삭제( 0 )">삭제</button>
-								<button onclick="삭제( 1 )">삭제</button>
-								<button onclick="삭제( 2 )">삭제</button>
-							*/ 
+					<td>${ 대여목록.includes( 도서목록[i] ) ? '대여중' : '대여가능' } </td>
+				`
+		if( 타입 == 1 ){ // 만약에 함수의 인수가 타입 1 이면 관리자 쪽 테이블  
+			html +=  `<td><button onclick="삭제( ${i} )">삭제</button></td><tr>`
+			// 3. 마크업에 html 대입
+			document.querySelector('.관리자테이블').innerHTML = html;
+		}else if( 타입 == 2 ){ // 만약에 함수의 인수가 타입 2 이면 고객 쪽 테이블 
+			html +=  `<td><button onclick="대여( ${i} )">대여</button>
+						<button onclick="반납( ${i} )">반납</button>
+						<tr>`
+			document.querySelector('.고객테이블').innerHTML = html;
+		}
 	}
-	// 3. 마크업에 html 대입
-	document.querySelector('.관리자테이블').innerHTML = html;	
 }
 // 3. 도서 삭제 함수 
 function 삭제( i ){
@@ -65,7 +67,34 @@ function 삭제( i ){
 	// 2. 삭제처리 
 	도서목록.splice( i , 1 )
 	// 3. 렌더링[화면 업데이트]
-	도서현황()
+	도서현황( 1 );도서현황( 2 );
+}
+
+// 5. 대여 함수 [ 대여중일때 불가능 ] 대여목록에서 도서명 추가 
+function 대여( i ){
+	// 1. 대여 체크 
+	if( 대여목록.includes( 도서목록[i]) ){
+		alert('대여중인 도서 입니다. 불가능 ')
+		return false;
+	}
+	// 2. 대여
+	대여목록.push( 도서목록[i] )
+	// 3. 렌더링 [ 화면 업데이트 ]
+	도서현황( 1 );도서현황( 2 );
+	return true;
+}
+// 6. 반납 함수 [ 대여중이 아닐때 불가능 ] 대여목록에서 도서명 삭제 
+function 반납( i ){ 
+	// 1. 반납 체크 
+	if( 대여목록.includes(도서목록[i]) ){
+		// 2. 반납
+		대여목록.splice( i , 1 )
+		//3. 렌더링 
+		도서현황( 1 );도서현황( 2 );
+		return true;
+	}
+	alert('대여중인 도서가 아닙니다. 불가능 ')
+	return false;
 }
 
 
@@ -79,6 +108,29 @@ function 삭제( i ){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <button onclick="삭제( ${i} )">삭제</button> : js가 버튼를 생성하기 전
+/*
+	js가 버튼 생성한 후
+	<button onclick="삭제( 0 )">삭제</button>
+	<button onclick="삭제( 1 )">삭제</button>
+	<button onclick="삭제( 2 )">삭제</button>
+*/ 
 
 
 
