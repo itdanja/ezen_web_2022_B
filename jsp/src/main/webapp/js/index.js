785c5c0a9d270394f75a7e879ad6545207f44143
이젠강사 <itdanja@kakao.com>
2023-01-26 17:54:47 +0900

    day17 할인율 계산후 parseInt

--- jsp/src/main/webapp/js/index.js
+++ jsp/src/main/webapp/js/index.js
@@ -5,7 +5,7 @@ let productList = [
 	{ img : 'p3.gif' , title : '로라 오프 숄더 셔츠' , size : '[ M , L ]' , price : 1563300 , discount : 0.9 , like : 45 , review : 4 } , 
 	{ img : 'p4.gif' , title : '라움 리본 포켓 피치기모 여리핏 셔츠 원피스' , size : '[ FREE ]' , price : 8000 , discount : 0.5 , like : 10 , review : 20 } , 
 	{ img : 'p5.gif' , title : '모앤 꽈배기 V넥 루즈핏 가디건' , size : '[ M , L ]' , price : 123500 , discount : 0.4 , like : 8 , review : 412 } , 
-	{ img : 'p6.gif' , title : '블린 랩 버튼 하프밴딩 피치기모 카라 원피스' , size : '[ FREE ]' , price : 789000 , discount : 0.2 , like : 32 , review : 780 } 
+	{ img : 'p6.gif' , title : '블린 랩 버튼 하프밴딩 피치기모 카라 원피스' , size : '[ FREE ]' , price : 54000 , discount : 0.28 , like : 32 , review : 780 } 
 ]
 
 product_print();
@@ -13,6 +13,7 @@ product_print();
 function product_print(){
 	let html = ``
 	productList.forEach( ( o , i ) => { 
+		console.log( o.discount*100  )
 		html += `
 			<div class="item"> 					<!-- 제품 1개 -->
 				<img src="img/${ o.img }"><!-- 제품이미지 -->
@@ -21,8 +22,8 @@ function product_print(){
 					<div class="item_size">${ o.size }</div>	<!-- 제품사이즈 -->
 					<div class="item_price">${ o.price.toLocaleString() } 원</div>	<!-- 원가 -->
 					<div>
-						<span class="item_sale">${ ( o.price - (o.price*o.discount) ) .toLocaleString() } 원</span>	<!-- 판매가 -->
-						<span class="item_discount">${ o.discount*100 }%</span>	<!-- 할인율 -->
+						<span class="item_sale">${ ( o.price - parseInt(o.price*o.discount) ) .toLocaleString() } 원</span>	<!-- 판매가 -->
+						<span class="item_discount">${ parseInt(o.discount*100) }%</span>	<!-- 할인율 -->
 					</div>
 				</div>
 				<div class="item_bottom">	<!-- 제품정보 하단 구역  -->
