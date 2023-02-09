fab4d075174a342acda3af8746d74078dc69ff9f
이젠강사 <itdanja@kakao.com>
2023-02-09 14:56:36 +0900

    day27 회원관리 arraylist

--- backend/src/Day07/Ex4/Ex4_회원시스템_객체.java
+++ backend/src/Day07/Ex4/Ex4_회원시스템_객체.java
@@ -33,12 +33,26 @@ public class Ex4_회원시스템_객체 { // class s
 						member.name = inputname;		// 2. 객체에 입력받은 값을 해당 필드에 대입 
 						member.phone = inputphone;		// 3. 객체에 입력받은 값을 해당 필드에 대입 
 						memberList[i] = member;			// 4. 객체를 배열에 저장 
+						System.out.println(i+"번 회원이 등록 되었습니다. 정보 : " + memberList[i] );
 						break;
 					}
 				}
 				
 			}
-			else if( ch == 2 ) { }
+			else if( ch == 2 ) {
+				System.out.print(" 삭제할 번호/인덱스 : ");
+				int no = scanner.nextInt();
+				// 3. 배열내 데이터 삭제 // splice 기능 [ 배열내 null 만들기 ]
+				memberList[ no ] = null;
+				// * 삭제후 빈자리 채우기 [ 삭제된 인덱스 뒤로 한칸씩 당기기 ]
+				for( int i = no ; i<memberList.length ; i++ ) {
+					if( i+1 == memberList.length ) { // 마지막 인덱스 이면 
+						memberList[i] = null; break;	// 마지막 인덱스 null 넣고 나가기 
+					}
+					memberList [ i ] = memberList[ i+1 ];
+					if( memberList[i+1] == null ) break; // 다음 인덱스가 null 이면 종료 
+				}
+			}
 			else { }
 		} // while e 
 	} // main e 
---
+++ backend/src/Day07/Ex6/Member.java
@@ -0,0 +1,6 @@
+package Day07.Ex6;
+
+public class Member {
+	String name;
+	String phone;
+}
