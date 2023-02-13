ee0d29d789133ee10177c007df94f9bd3809197d
이젠강사 <itdanja@kakao.com>
2023-02-13 15:58:17 +0900

    day29 MVC 싱글톤 적용

---
+++ backend/src/Day09/Ex10_게시판/view/Front.java
@@ -0,0 +1,40 @@
+package Day09.Ex10_게시판.view;
+
+import java.util.Scanner;
+
+public class Front {
+	
+	// 1. 싱글톤 객체 [ 1. 프로그램내 하나의 객체 - 공유 메모리 ]
+	private static Front front = new Front();
+	private Front() {}
+	public static Front getInstance() {
+		return front;
+	}
+	
+	// 필드  : 입력 객체 
+	private Scanner scanner = new Scanner(System.in);
+	
+	// 2. index 함수 
+	public void index() {
+		while( true ) {
+			System.out.print(" 1.쓰기 : ");
+			int ch = scanner.nextInt(); // 지역변수 
+			if( ch == 1 ) { write_page(); }
+		}
+	}
+	
+	// 3. 쓰기 페이지 함수 
+	private void write_page() {
+		System.out.println(" --------- 글 쓰기 페이지 ------------");
+	}
+	
+	// 4. 출력 페이지 함수 
+	private void print_page() {
+		
+	}
+	
+}
+
+
+
+
