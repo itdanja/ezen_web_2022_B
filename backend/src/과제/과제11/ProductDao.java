517432786e2c95a37b8a733545b384a961232254
이젠강사 <itdanja@kakao.com>
2023-02-22 14:09:47 +0900

    day36 과제11

---
+++ backend/src/과제/과제11/ProductDao.java
@@ -0,0 +1,30 @@
+package 과제.과제11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ProductDao {
+
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	
+	private static ProductDao dao = new ProductDao();
+	private ProductDao() {
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/과제11","root","1234");
+		}catch (Exception e) { System.out.println( e ); }
+	}
+	public static ProductDao getInstance() { return dao; }
+	
+}
+
+
+
+
+
+
+
+
---
+++ backend/src/과제/과제11/ProductDto.java
@@ -0,0 +1,59 @@
+package 과제.과제11;
+
+public class ProductDto {
+	
+	private int  pno;
+    private String  pname;
+    private int pprice;
+    private int  pstock;
+    
+    public ProductDto() {}
+
+	public ProductDto(int pno, String pname, int pprice, int pstock) {
+		super();
+		this.pno = pno;
+		this.pname = pname;
+		this.pprice = pprice;
+		this.pstock = pstock;
+	}
+
+	@Override
+	public String toString() {
+		return "ProductDto [pno=" + pno + ", pname=" + pname + ", pprice=" + pprice + ", pstock=" + pstock + "]";
+	}
+
+	public int getPno() {
+		return pno;
+	}
+
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public int getPprice() {
+		return pprice;
+	}
+
+	public void setPprice(int pprice) {
+		this.pprice = pprice;
+	}
+
+	public int getPstock() {
+		return pstock;
+	}
+
+	public void setPstock(int pstock) {
+		this.pstock = pstock;
+	}
+    
+	
+	
+}
