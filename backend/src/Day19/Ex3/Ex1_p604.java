be053ebfd92d49895dddb449a98dd3140ac728ec
이젠강사 <itdanja@kakao.com>
2023-02-27 14:09:18 +0900

    day39 멀티스레드

--- backend/src/Day19/Ex3/Ex1_p604.java
+++ backend/src/Day19/Ex3/Ex1_p604.java
@@ -31,16 +31,29 @@ public class Ex1_p604 {
 //		sumThread.start();	// 계산시작 
 //		System.out.println( sumThread.getSum() ); // 결과출력
 		
-		SumThread sumThread = new SumThread();
-		sumThread.start(); // 계산시작 
-		try {
-			sumThread.join(); // --현스레드(main) 와 조인[합치기]
-		}catch (Exception e) {	}
-		System.out.println( sumThread.getSum() );
-//		
+//		SumThread sumThread = new SumThread();
+//		sumThread.start(); // 계산시작 
+//		try {
+//			sumThread.join(); // --현스레드(main) 와 조인[합치기]
+//		}catch (Exception e) {	}
+//		System.out.println( sumThread.getSum() );
+		
+		// 4. 
+		WorkThread workThreadA = new WorkThread("workThreadA");	// 스레드A 객체 생성 
+		WorkThread workThreadB = new WorkThread("workThreadB");	// 스레드B 객체 생성 
+		workThreadA.start();	// 스레드A 실행 
+		workThreadB.start();	// 스레드B 실행 	
+		
+		try { Thread.sleep(5000); } catch (Exception e) { }	// 5초간 일시정지 
+		workThreadA.work = false;	// 스레드A 필드 변경 스레드 양보상태
+		try { Thread.sleep(5000); } catch (Exception e) { }	// 5초간 일시정지 
+		workThreadA.work = true;	// 스레드A 필드 변경 스레드 대기상태
+		
 	}
-	
 }
+
+
+
 
 
 
