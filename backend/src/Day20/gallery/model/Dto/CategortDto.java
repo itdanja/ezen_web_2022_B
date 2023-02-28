8f0f71cd48a7168f54c455a32b617abe80db928b
이젠강사 <itdanja@kakao.com>
2023-02-28 11:04:26 +0900

    day30 dto 구현

--- backend/src/Day20/gallery/model/Dto/CategortDto.java
+++ backend/src/Day20/gallery/model/Dto/CategortDto.java
@@ -1,5 +1,37 @@
 package Day20.gallery.model.Dto;
 
 public class CategortDto {
+	private int cno;
+	private String cname;
+	
+	public CategortDto() {}
 
+	public CategortDto(int cno, String cname) {
+		super();
+		this.cno = cno;
+		this.cname = cname;
+	}
+
+	@Override
+	public String toString() {
+		return "CategortDto [cno=" + cno + ", cname=" + cname + "]";
+	}
+
+	public int getCno() {
+		return cno;
+	}
+
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+    
 }
