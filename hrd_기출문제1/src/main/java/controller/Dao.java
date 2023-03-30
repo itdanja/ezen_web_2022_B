ace2b3bd43b1966898c26cc5bef3afb714055c9e
이젠강사 <itdanja@kakao.com>
2023-03-30 10:59:02 +0900

    day61 hrd기출문제1 기본레이아웃 , db입력

---
+++ hrd_기출문제1/src/main/java/controller/Dao.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Dao {
+	
+	public Connection con;
+	public PreparedStatement ps;
+	public ResultSet rs;
+	
+	public static Connection getConnection() throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/xe","root","1234");
+		return con;
+	}
+	public Dao() {
+		try {
+			con = getConnection();
+		}catch (Exception e) {System.out.println(e);}
+	}
+}
