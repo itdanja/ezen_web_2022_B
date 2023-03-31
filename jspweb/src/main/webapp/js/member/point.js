402f5c89cc0394ea6ee6aa91c1f22664036935a0
이젠강사 <itdanja@kakao.com>
2023-03-31 10:21:55 +0900

    day62 포트원api 이용한 결제시스템 사용

---
+++ jspweb/src/main/webapp/js/member/point.js
@@ -0,0 +1,41 @@
+let pay = 0; // 결제 금액 
+
+function setpay( p ){
+	pay = p ;
+	alert('결제금액 선택' + p );
+}
+
+var IMP = window.IMP;   // 생략 가능
+IMP.init("imp35631338"); // 예: imp00000000 
+
+function requestPay() {
+	if( pay == 0 ){ 
+		alert('충전할 금액을 선택해주세요.');
+		return;
+	}
+    IMP.request_pay({
+      pg: "kakaopay", // 각 pg별 이름존재
+      pay_method: "card",	// pg별 결제방식
+      merchant_uid: "ORD20180131-0000011",   // 주문번호 // 결제DB에서 사용할 PK
+      name: "이젠 포인트 결제",	// 상품명
+      amount: pay,                         // 숫자 타입 // 결제금액
+      buyer_email: "gildong@gmail.com",		// 관리자 정보
+      buyer_name: "홍길동",
+      buyer_tel: "010-4242-4242",
+      buyer_addr: "서울특별시 강남구 신사동",
+      buyer_postcode: "01181"
+    }, function (rsp) { // callback
+      if (rsp.success) { // 결제성공
+        console.log(rsp )
+      } else { // 결제취소/실패 
+        // 결제 성공 했다는 가정 
+        
+      }
+    });
+  }
+  
+  
+  
+  
+  
+  
---
+++ jspweb/src/main/webapp/member/point.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<%@include file="/header.jsp" %>
+
+	<div class="container">
+		
+		
+		<h3> 포인트결제</h3>
+		<button type="button" onclick="setpay(10000)"> 10000 원 </button>
+		<button type="button" onclick="setpay(50000)"> 50000 원 </button>
+		<br>
+		<button type="button" onclick="requestPay()">카드결제</button>
+	</div>
+	
+	<!-- 포트원 결제 JS -->
+	<script src="https://cdn.iamport.kr/v1/iamport.js"></script>
+	
+	<script src="/jspweb/js/member/point.js" type="text/javascript"></script>
+	
+
+</body>
+</html>
